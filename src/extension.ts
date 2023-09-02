9db53da58376ba8b388139562d434b9c347d1113
Ian Luan <ian.oliveira@lais.huol.ufrn.br>
2023-09-02 11:20:26 -0300

    feat: add grammar injection to recognize slippers block components

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import { ExtensionContext } from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext) {
+	
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
